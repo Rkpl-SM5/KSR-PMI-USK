6a725c0e3bb825fa15d6af7c62a4baa81968bd02
Zik <muammar.clasic@gmail.com>
2018-12-03 01:09:48 +0700

    add module gallery
    module untuk menu gallery

---
+++ application/modules/galleryDashboard/views/galleryDashboard.php
@@ -0,0 +1,12 @@
+<div class="row">
+  <div class="col-lg-12">
+      <div class="card">
+          <div class="card-body">
+              <!-- Search  -->
+              <h1>Gallery</h1>
+              <input name="Golongan Darah"/>
+          </div>
+      </div>
+  </div><!-- /# column -->
+</div>
+

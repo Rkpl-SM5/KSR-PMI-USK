c12823f7731ac2ddda2e1ba508af3e6ec5bdac69
Zik <muammar.clasic@gmail.com>
2018-12-03 01:10:14 +0700

    add moduie donor
    module untuk menu donor

---
+++ application/modules/donorDashboard/views/donorDashboard.php
@@ -0,0 +1,13 @@
+
+      <div class="row">
+          <div class="col-lg-12">
+              <div class="card">
+                  <div class="card-body">
+                      <!-- Search  -->
+                      <h1>Donor</h1>
+                      <input name="Golongan Darah"/>
+                  </div>
+              </div>
+          </div><!-- /# column -->
+      </div>
+

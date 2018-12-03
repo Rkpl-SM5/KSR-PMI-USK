f8e1f1897f56fd59f02c75b36197a1cf30f7408f
Zik <muammar.clasic@gmail.com>
2018-12-03 12:02:30 +0700

    edit module statistics

--- application/modules/statisticsDashboard/views/allTheTimeDashboard.php
+++ application/modules/statisticsDashboard/views/allTheTimeDashboard.php
@@ -1,13 +1,10 @@
+<div class="col-lg-12">
+    <div class="card">
+        <div class="card-body"><div style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;" class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+            <h4 class="mb-3">Bar chart </h4>
+            <canvas id="barChart" style="display: block; width: 100%; height: 226px;" width="452" height="226" class="chartjs-render-monitor"></canvas>
+        </div>
+    </div>
+</div>
 
-      <div class="row">
-          <div class="col-lg-12">
-              <div class="card">
-                  <div class="card-body">
-                      <!-- Search  -->
-                      <h1>all stats</h1>
-                      <input name="Golongan Darah"/>
-                  </div>
-              </div>
-          </div><!-- /# column -->
-      </div>
 

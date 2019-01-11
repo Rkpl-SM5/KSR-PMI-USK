86e59ecb2fad6f7f9baf548a65c1127ec3a3bc00
Andika Pratama <apratama918@gmail.com>
2019-01-11 15:20:34 +0700

    dashboard php

---
+++ application/modules/aboutDashboard/views/tes.php
@@ -0,0 +1,15 @@
+<div vocab="http://schema.org/" typeof="Product">
+    <span property="name">Iphone 6</span>
+    	<img src="iphone-6.jpg" alt='Iphone 6 4G LTE' />
+    <div property="offers"  typeof="Offer">
+      <!--Harga 15juta, a number, with locale-specific thousands separator
+      and decimal mark, and the $ character is marked up with the
+      machine-readable code "USD" lihat iso 4217-->
+    	<span property="priceCurrency" content="IDR">Rp.</span>
+        <span property="price" content="15000000">15.000.000,00</span>
+    	<link property="availability" href="http://schema.org/InStock" />Stok ada
+    </div>
+	Deskripsi Produk:
+    <span property="description">Bersiaplah untuk iPhone tertipis, teringan dan tercepat, iPhone 6. Prosesor canggih, kamera menakjubkan, desain menawan, dan berbagai fitur lainnya telah ditanamkan pada smartphone ini. Penantian telah berakhir, smartphone yang begitu dinantikan dan hadir dengan apa yang Anda idamkan dari sebuah smartphone, sudah ada di depan mata Anda.</span>
+Customer reviews:
+</div>

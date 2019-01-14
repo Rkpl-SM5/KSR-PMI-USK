f5e00f00d62736043c8578d9c3c6f127589242b9
Unknown <apratama918@gmail.com>
2019-01-14 12:30:25 +0700

    Add about

--- application/modules/aboutDashboard/views/aboutDashboard.php
+++ application/modules/aboutDashboard/views/aboutDashboard.php
@@ -1,13 +1,66 @@
 
-      <div class="row">
-          <div class="col-lg-12">
-              <div class="card">
-                  <div class="card-body">
-                      <!-- Search  -->
-                      <h1>About</h1>
-                      <textarea rows="5" cols="70" ></textarea>
-                  </div>
-              </div>
-          </div><!-- /# column -->
-      </div>
+<div style="margin:0 auto;" class="card col-md-9">
+    <div class="card-header">
+        <h4>About</h4>
+    </div>
+    <div class="card-body">
+        <div class="custom-tab">
 
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link  active show" id="custom-nav-home-tab" data-toggle="tab" href="#custom-nav-home" role="tab" aria-controls="custom-nav-home" aria-selected="false">History</a>
+                    <a class="nav-item nav-link" id="custom-nav-profile-tab" data-toggle="tab" href="#custom-nav-profile" role="tab" aria-controls="custom-nav-profile" aria-selected="false">Vision & Mision</a>
+                    <a class="nav-item nav-link" id="custom-nav-contact-tab" data-toggle="tab" href="#custom-nav-contact" role="tab" aria-controls="custom-nav-contact" aria-selected="true">Contact</a>
+                </div>
+            </nav>
+            <div class="tab-content pl-3 pt-2" id="nav-tabContent">
+                <div style="text-indent: 30px; text-align: justify;" class="tab-pane fade active show" id="custom-nav-home" role="tabpanel" aria-labelledby="custom-nav-home-tab">
+                   <br><h5 align="center">KSR PMI Unit 01 Universitas Syiah Kuala</h5><br>
+                   <p>Korps Sukarela Palang Merah Indonesia (KSR PMI) Unit Perguruan tinggi merupakan suatu unit didalam perhimpunan Palang Merah
+                      Indonesia yang merupakan wadah kegiatan atau wadah pengabdian bagi anggotanya yang memberikan pertolongan atau bantuan bagi
+                      yang membutuhkan dalam unit atau dimanapun saat terjadinya suatu bencana maupun musibah lainnya.</p>
+                    <p>Korps Sukarela Palang Merah Indonesia (KSR PMI) Unit 01 Universitas Syiah Kuala (Unsyiah) merupakan unit didalam perhimpunan
+                       Palang Merah Indonesia yang tergabung dalam Unit Kegiatan Mahasiswa (UKM) di kampus Universitas Syiah Kuala. Sruktur organisasi
+                       KSR PMI Unit 01 Unsyiah dibawah pembinaan Pembantu Rektor III Universitas Syiah Kuala dan Ketua PMI Cabang Kota Banda Aceh.</p>
+                    <p>KSR PMI Unit Unit 01 Unsyiah merupakan ujung tombak Palang Merah Indonesia dalam hal melakukan tugas – tugas kemanusiaan dalam
+                       membantu sesama. Tugas yang dilakukan selalu didasari oleh Tujuh Prinsip Dasar Gerakan Palang Merah dan Bulan Sabit Merah
+                       Internasional dan Tri Dharma Perguruan Tinggi. Tujuh Prinsip Dasar Gerakan Palang Merah dan Bulan Sabit Merah Internasional
+                       yaitu : Kemanusiaan, Kesamaan, Kenetralan, Kemandirian, Kesukarelaan, Kesatuan dan Kesemestaan. Prinsip yang diemban ini harus
+                       bisa mencapai sasaran yang dimaksud sehingga masyarakat bisa merasakan manfaatnya. Tri Dharma Perguruan Tinggi yaitu : Pendidikan,
+                       Penelitian dan Pengabdian. Serta pedoman teknis KSR PMI Unit 01 Unsyiah menggunakan Pedoman Manajemen Relawan KSR-TSR dan pedoman
+                       KSR PMI Unit 01 Universitas Syiah Kuala</p>
+                    <p>KSR PMI Unit 01 Unsyiah didirikan pada tanggal 4 Juni 1995 dengan pendiri nya bernama Kak Cut Faisal Syahputra dengan komandan pertama
+                       Kak Suburhan Pagan. Semenjak didirikan sampai sekarang tahun 2011 KSR PMI Unit 01 unsyiah telah melahirkan ribuan relawan yang telah
+                       membuktikan dedikasi dalam menjalankan misi kemanusiaan. Organisasi ini memiliki satu Unit Markas sebagai sekretariat di Gelanggang
+                       Mahasiswa Unsyiah dan memiliki Satu Unit Pos Pelayanan Pertama Pertama (P4) dan Gudang yang terletak di Tower Air kampus Unsyiah</p>
+                </div>
+                <div class="tab-pane fade" id="custom-nav-profile" role="tabpanel" aria-labelledby="custom-nav-profile-tab">
+                    <br><h5>Visi :</h5><br>
+                    <p>Relawan Unsyiah siap menyediakan dan memberikan pelayanan kepalangmerahan dengan cepat dan tepat.</p>
+                    <h5>Misi :</h5>
+                    <p>
+                      <ol style="margin-left:17px;">
+                        <li><p>Menyebarluaskan dan mendorong aplikasi secara konsisten Prinsip prinsip Dasar Palang Merah dan Bulan Sabit Merah Internasional.</p></li>
+                        <li><p>Berperan aktif dalam penanganan bahaya HIV/AIDS dan penyalahgunaan NAPZA di lingkungan kampus maupun masyarakat.</p></li>
+                        <li><p>Menggerakan generasi muda khususnya mahasiswa dan pelajar dalam tugas-tugas kemanusiaan.</p></li>
+                        <li><p>Pengembangan dan penguatan kapasitas organisasi di seluruh jajaran KSR-PMI guna meningkatkan kualitas potensi sumber daya manusia,
+                               sumber daya dan dana agar visi, misi dan kegiatan KSR- PMI dapat diwujudkan secara berkesinambung.</p></li>
+                        <li><p>Membantu PMI dalam menjalankan Tugas Kemanusiaan tidak terlepas dari tujuh prinsip dasar Palang Merah dan bulan Sabit merah
+                               internasional dan Tri Dharma Perguruan Tinggi.</p></li>
+
+
+                      </ol>
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="custom-nav-contact" role="tabpanel" aria-labelledby="custom-nav-contact-tab">
+                    <br><h5>Contact Us</h5><br>
+                    <p class="contact"><i class="fa fa-phone"></i> +62 823-6121-6207</p>
+                    <p class="contact"><i class="ti ti-themify-favicon-alt"></i> +62 823-6121-6207</p>
+                    <p class="contact"><i class="ti ti-instagram"></i> #ksrpmiunsyiah</p>
+                    <p class="contact"><i class="ti ti-email"></i> pmiunsyiah@gmail.com</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
